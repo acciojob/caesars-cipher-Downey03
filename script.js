da312f05f904e25bc3c45fbb691ea44b2439cf78
Downey03 <44365074+Downey03@users.noreply.github.com>
2023-06-15 11:22:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -42,7 +42,6 @@ function rot13(encodedStr) {
   return decodedArr; //return decodedArr
 }
 
-console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
